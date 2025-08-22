06b2e6ead7c6989bb07ee29aa4e2f4a85023b4a0
Kasun Bamunuarachchi <141222078+kasunbamu@users.noreply.github.com>
2025-08-22 09:23:41 +0530

    First commit

---
+++ UndoTest/UndoTest.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new UndoTest();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace UndoTest
+{
+    [ComVisible(true)]
+    public class UndoTest : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+        Word.UndoRecord undoRecord;
+
+        public UndoTest()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("UndoTest.UndoTest.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        public void OnTextButton(Office.IRibbonControl control)
+        {
+            Word.Range currentRange = Globals.ThisAddIn.Application.Selection.Range;
+
+            var application = Globals.ThisAddIn.Application;
+            var document = application.ActiveDocument;
+
+            this.undoRecord = application.UndoRecord;
+
+            this.undoRecord.StartCustomRecord("MyRecord");
+            string replacementText = "";
+
+            Word.Find find = currentRange.Find;
+            find.ClearFormatting();
+            find.Replacement.ClearFormatting();
+            find.Text = "^p"; // Paragraph break
+            find.Replacement.Text = replacementText;
+            find.Forward = true;
+            find.Wrap = Word.WdFindWrap.wdFindStop;
+            find.Format = false;
+            find.MatchCase = false;
+            find.MatchWholeWord = false;
+            find.MatchWildcards = false;
+            find.Execute(Replace: Word.WdReplace.wdReplaceAll);
+
+            foreach (Word.Paragraph paragraph in document.Paragraphs)
+            {
+                paragraph.Range.set_Style(Microsoft.Office.Interop.Word.WdBuiltinStyle.wdStyleEmphasis);
+            }
+
+            //WdListType listType = WdListType.wdListNoNumbering;
+            //int listLevelNumber = 1;
+
+            //listType = document.Paragraphs[0].Range.ListFormat.ListType;
+            //switch (listType)
+            //{
+            //    case WdListType.wdListBullet:
+            //    case WdListType.wdListPictureBullet:
+            //        document.Paragraphs[0].Range.ListFormat.ApplyBulletDefault(
+            //        DefaultListBehavior: Word.WdDefaultListBehavior.wdWord10ListBehavior
+            //        );
+            //        break;
+
+            //    case WdListType.wdListSimpleNumbering:
+            //        document.Paragraphs[0].Range.ListFormat.ApplyNumberDefault(
+            //        DefaultListBehavior: Word.WdDefaultListBehavior.wdWord10ListBehavior
+            //        );
+            //        break;
+
+            //    case WdListType.wdListOutlineNumbering:
+            //        document.Paragraphs[0].Range.ListFormat.ApplyOutlineNumberDefault(
+            //        DefaultListBehavior: Word.WdDefaultListBehavior.wdWord10ListBehavior
+            //        );
+            //        break;
+
+            //    case WdListType.wdListMixedNumbering:
+            //        document.Paragraphs[0].Range.ListFormat.ApplyListTemplateWithLevel(
+            //        document.Paragraphs[0].Range.ListFormat.ListTemplate,
+            //        ContinuePreviousList: true,
+            //        ApplyTo: Word.WdListApplyTo.wdListApplyToSelection
+            //        );
+            //        break;
+            //}
+            //document.Paragraphs[0].Range.ListFormat.ListLevelNumber = listLevelNumber;
+
+            this.undoRecord.EndCustomRecord();
+
+        }
+
+        public void OnTableButton(Office.IRibbonControl control)
+        {
+
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
